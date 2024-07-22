3dbdab47f593af3d46069a647c07aeef993d385c
THINH110502 <thinhle110502@gamil.com>
2024-07-22 17:26:21 +0700

    init projet

---
+++ 123/js/login.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var eye = document.getElementById('eye');
+    eye.addEventListener('click', function() {
+        this.classList.toggle('open');
+        var icon = this.querySelector('i');
+        icon.classList.toggle('fa-eye');
+        icon.classList.toggle('fa-eye-slash');
+        var passwordInput = this.previousElementSibling;
+        if (this.classList.contains('open')) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    });
+});
