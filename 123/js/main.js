68da08478a057aa501ab616c7b090c8200b309ff
THINH110502 <thinhle110502@gamil.com>
2024-07-24 16:14:01 +0700

    new

---
+++ 123/js/main.js
@@ -0,0 +1,35 @@
+const header = document.querySelector("header")
+window.addEventListener("scroll", function(){
+    x = window.pageYOffset
+    if(x > 0){
+        header.classList.add("sticky")
+    }else{
+        header.classList.remove("sticky")
+    }
+})
+
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
+const imgContainer = document.querySelector(".aspect-ratio-169")
+const dotItem = document.querySelectorAll(".dot")
+let imgNumber = imgPosition.length
+let index = 0
+imgPosition.forEach(function(images,index){
+    images.style.left = index*100 + "%"
+    dotItem[index].addEventListener("click",function(){
+        backgroud(index)
+    })
+})
+function imgBackgroud (){
+    index++;
+    if (index >=imgNumber){
+        index = 0
+    }
+    backgroud(index)
+}
+function backgroud (index){
+    imgContainer.style.left = "-" +index*100+ "%"
+    const dotActive = document.querySelector(".active")
+    dotActive.classList.remove("active")
+    dotItem[index].classList.add("active")
+}
+setInterval(imgBackgroud, 5000)
