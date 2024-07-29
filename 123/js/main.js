6585285107515ca55e0c04f70fbe18c98cb76833
THINH110502 <thinhle110502@gamil.com>
2024-07-29 16:44:48 +0700

    new

--- 123/js/main.js
+++ 123/js/main.js
@@ -40,3 +40,33 @@ itemsLiderbar.forEach(function(menu,index){
     menu.classList.toggle("block")
     })
 })
+// ---------------------------product bao quan -----------------
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smalImg = document.querySelectorAll(".product-content-left-small-img img")
+smalImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click", function(){
+        bigImg.src = imgItem.src
+    })
+})
+
+const chitiet = document.querySelector(".baoquan")
+const baoquan = document.querySelector(".chitiet")
+if(baoquan){
+    baoquan.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "none"
+    })
+}
+if(chitiet){
+    chitiet.addEventListener("click", function(){
+        document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
+        document.querySelector(".product-content-right-bottom-content-baoquan").style.display = "block"
+    })
+}
+
+const butTon = document.querySelector(".product-content-right-bottom-top")
+if(butTon){
+    butTon.addEventListener("click", function(){
+        document.querySelector("product-content-right-bottom-content-big").classList.toggle("activeB")
+    })
+}
