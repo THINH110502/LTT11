7ae37f31ee0277d550f0998b5c209940d8205c0c
THINH110502 <thinhle110502@gamil.com>
2024-08-14 15:29:12 +0700

    ness

--- ABC/js/main.js
+++ ABC/js/main.js
@@ -1,8 +1,17 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
-let formClose = document.querySelector('#form-close');
-let menu = document.querySelector('#menu-bar');
+const searchBtn = document.querySelector('#search-btn');
+const searchBar = document.querySelector('.search-bar-container');
+const menu = document.querySelector('#menu-bar');
+const formBtn = document.querySelector('#login-btn');
+const loginForm = document.querySelector('.login-form-container');
+const formClose = document.querySelector('#form-close');
 
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
 
 menu.addEventListener('click', () =>{
     menu.classList.toggle('fa-times');
@@ -13,3 +22,26 @@ searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
 });
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
+});
+
+
+const videoBtns = document.querySelectorAll('.vid-btn');
+const videoSlider = document.getElementById('video-slider')
+videoBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        videoBtns.forEach(btn => btn.classList.remove('active'));
+        this.classList.add('active');
+        const newSrc = this.getAttribute('data-src');
+        videoSlider.src = newSrc;
+        videoSlider.play();
+    });
+});
+
+
